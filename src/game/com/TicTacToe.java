312ddaf985393f9397c85f8f33a8e89fe1bf7dda
kiranudhan <udhankiran62@gmail.com>
2023-01-02 14:25:41 +0530

    choose the letter

--- src/game/com/TicTacToe.java
+++ src/game/com/TicTacToe.java
@@ -1,10 +1,48 @@
 package game.com;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+	static char userChoice;
+	static char computerChoice;
+	char[] board=new char[10];
+	Scanner sc=new Scanner(System.in);
 	
-	public static void main(String[] args) {
-		System.out.println("Welcome to TicTacToeGame");
+	private void createBoard() {
+		
+		for(int i=1;i<10;i++) {
+			board[i]=' ';
+		}
 		
 	}
+	
+	private void chooseLetter() {
+		System.out.println("choose your choice 1. X  and 2.o");
+		int choice=sc.nextInt();
+		switch(choice){
+		case 1:
+			userChoice='X';
+			computerChoice='o';
+			System.out.println("user choice is :: "+userChoice+" Computer choice is :: "+computerChoice);
+			break;
+		case 2:
+			userChoice='o';
+			computerChoice='X';
+			System.out.println("user choice is :: "+userChoice+" Computer choice is :: "+computerChoice);
+			break;
+		default:
+			System.out.println("Enter valid option");
+			chooseLetter();
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to TicTacToeGame");
+		TicTacToe obj=new TicTacToe();
+        obj.createBoard();
+        obj.chooseLetter();
+	}
+
+
 
 }
